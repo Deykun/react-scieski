9101b45d8726ab8296d665249312320acd11688d
Szymon T <stdeykun+git@gmail.com>
2019-03-14 22:14:24 +0100

    Project initiation

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import UniqueId from 'react-html-id';  
+import { BrowserRouter as Router, Link, NavLink, Redirect, Prompt } from 'react-router-dom';
+import Route from 'react-router-dom/Route';
 
 class App extends Component {
+
+  constructor() {
+    super();
+
+    UniqueId.enableUniqueIds(this);
+
+    this.state = {
+      routes: [
+        { id: this.nextUniqueId() },
+        { id: this.nextUniqueId() },
+      ]
+    }
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      
+      <Router>
+        <div className="App">
+          <h1><NavLink to="/editor/" activeClassName="active">Scieski</NavLink></h1>
+
+          <aside id="scieski-editor">
+            <nav>
+              <NavLink to="/editor/routes/" exact activeClassName="active">Trasy</NavLink>
+              <NavLink to="/editor/settings/" activeClassName="active">Ustawienia</NavLink>
+            </nav>
+            <div>
+              <Route path="/routes/"
+                  render={
+                    () => {
+                      return( <div>Trasy</div> );
+                    }
+                  }
+              />
+
+            <Route path="/routes/:routename" exact strict render={ 
+              ( {match} ) => {
+
+                const indexOfRoute = this.state.tracks.findIndex( (route) => { return match.params.routename.toLowerCase() === route.routename.toLowerCase() ? true : false } ); 
+                return indexOfRoute >= 0 ? <div user={this.state.users[indexOfRoute]}>Trasa.</div> : <div>Brak trasy.</div>;
+              }
+            } 
+            /> 
+              <section id="routes">                
+              </section>
+            </div>
+          </aside>
+
+          
+        </div>
+      </Router>
     );
   }
 }
