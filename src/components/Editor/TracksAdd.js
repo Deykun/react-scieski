1c318d5596f89cdc0bdf579c961aa3f327a087a8
Szymon T <stdeykun+git@gmail.com>
2020-03-01 21:39:56 +0100

    Upload improvments 2k+ tracks loaded in 2.5min instead of 6

--- src/components/Editor/TracksAdd.js
+++ src/components/Editor/TracksAdd.js
@@ -1,9 +1,10 @@
 import React, {useCallback} from 'react'
 import moment from 'moment'
+import pLimit from 'p-limit'
 
 import { useDispatch } from 'react-redux'
 import { multipleActions } from '../../actions/'
-import { addTrack, refreshTracksSummary, saveTracks } from '../../actions/tracks'
+import { refreshTracksSummary, saveTracks } from '../../actions/tracks'
 import { addNotification, updateNotification } from '../../actions/notifications'
 
 import { v4 } from 'node-uuid'
@@ -11,14 +12,12 @@ import { useDropzone } from 'react-dropzone'
 
 import { DropZoneContainer } from '../../styles/components/Editor/TracksAdd.js'
 
-import { divideIntoSmallerArrays, checkFileMetadata, forEachPromise } from '../../utils/helpers'
+import { checkFileMetadata } from '../../utils/helpers'
 import { readFile } from '../../utils/tracks.js'
 
 const processFiles = (dispatch, files) => {
 /* One file is treated the same way */
   if ( !Array.isArray( files ) ) { files = [files] }
-
-  const startTime = new Date()
 
   const progressNotificationId = v4()
   dispatch( addNotification({ data: {
@@ -30,65 +29,67 @@ const processFiles = (dispatch, files) => {
     message: 'Tylko pliki w odpowiednim formacie zostaną dodane.'
   }}) )
 
-  files = divideIntoSmallerArrays(files, 500)
-
-  files = files.map( ( fileArr ) => {
-    const actions = []
-    const newFileArr = fileArr.filter( ( file ) => {
-      const fileMetadata = checkFileMetadata( file )
-      if ( fileMetadata.isFormatAllowed ) {
-        file.id = v4()
-        file.format = fileMetadata.data.format
-        actions.push( addTrack({ data: { id: file.id, status: 'loading', ...fileMetadata.data } } ) )
-      } else {
-        actions.push( addNotification({ id: v4(), 
-          code: 'FILE_FORMAT_REJECTED',
-          data: {
-            title: `Pominięto plik .${fileMetadata.data.format}`,
-            subtitle: 'nieobsługiwane roszerzenie',
-            message: `Plik: ${fileMetadata.data.name} został pominięty.`,
-          }
-        }))
-      }
-      return fileMetadata.isFormatAllowed
-    })
-    dispatch( multipleActions( actions ) )
+  const limit = pLimit(30)
   
-    return newFileArr
-  })
-
-  files = files.flat(1)
-
-  dispatch( updateNotification({ id: progressNotificationId, data: {
-    title: 'Importowanie',
-    subtitle: `${files.length} plików`,
-    message: 'Analizowanie plików z trasami plików.'
-  }}) )
+  const startTime = new Date()
 
   let processed = 0
-  const updateProgress = ( action ) => {
+  const updateProgress = () => {
     processed = processed + 1
-    const progress = ( (processed / files.length ) * 100).toFixed(1)
-    dispatch( updateNotification({ id: progressNotificationId, data: {
+    const progress = Math.round( (processed / files.length ) * 100)
+
+    return updateNotification({ id: progressNotificationId, data: {
       title: `Importowanie ${progress}%`,
       subtitle: `${files.length} plików`,
       percent: progress,
       message: `Sprawdzono ${processed} pliki.`
-    }}) )
-    dispatch( action )
+    }})
   }
 
-  forEachPromise( files, readFile, updateProgress ).then(() => {
+  let actions = []
+  let promises = files.map( file => {
+    return limit(() => new Promise( resolve => {
+      const processFile = async (file) => {
+        const fileMetadata = checkFileMetadata( file )
+        if ( !(fileMetadata.isFormatAllowed) ) {
+          actions.push( addNotification({ id: v4(), 
+            code: 'FILE_FORMAT_REJECTED',
+            data: {
+              title: `Pominięto plik .${fileMetadata.data.format}`,
+              subtitle: 'nieobsługiwane roszerzenie',
+              message: `Plik: ${fileMetadata.data.name} został pominięty.`,
+            }
+          }))
+        } else {
+          file.id = v4()
+          file.format = fileMetadata.data.format
+          const addTrack = await readFile( file )
+          actions.push( { id: file.id, ...addTrack, track: { ...fileMetadata.data, ...addTrack.track } } )
+        }
+        actions.push( updateProgress() )
+        if ( actions.length > 12 ) {
+          await dispatch( multipleActions( actions ) )
+          actions = []
+        }
+        resolve()
+      }
+      processFile( file )
+    }))
+  })
+
+  Promise.all(promises).then( async () => {
+    await dispatch( multipleActions( actions ) )
+
     const endTime = new Date()
     const performenceInMs = moment(endTime).diff(moment(startTime))
-    const performenceInSec = Math.floor( performenceInMs / 1000 )
     const performenceInMin = Math.floor( performenceInMs / 1000 / 60 )
+    const performenceInSec = Math.floor( ( performenceInMs - ( performenceInMin * 1000 * 60 ) ) / 1000 )
 
     dispatch( updateNotification({ id: progressNotificationId, data: {
       title: `Zaimportowano ${files.length} plików`,
       type: 'success',
       subtitle: '',
-      message: `Trasy zostały dodane w ${performenceInMin > 0 ? `${performenceInMin} minut` : ''}${performenceInSec} sekund.`
+      message: `Trasy zostały dodane w ${performenceInMin > 0 ? `${performenceInMin} minut` : ''} ${performenceInSec} sekund.`
     }}) )
     dispatch( refreshTracksSummary() )
     dispatch( saveTracks() )
@@ -112,7 +113,6 @@ const TracksAdd = () => {
         <p>Upuść plik lub pliki z trasami.</p> :
         <p>Przeciągnij i upuść trasy (.<strong>tcx</strong> lub .<strong>gpx</strong>) <br/>lub wybierz pliki klikając <strong className="positive">tutaj</strong>.</p>
       }
-      <p id="bla" onClick={()=>console.log('sdd11s')}>tt</p>
     </DropZoneContainer>
   )
 } 
