47c8603504e23729d2560fb942946172dcf0d722
Szymon T <stdeykun+git@gmail.com>
2020-03-20 13:41:33 +0100

    Loading app placeholder

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
-import React, { Suspense } from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom'
 import Scieski from './components/Scieski'
 import StoreProvider from './components/Wrappers/StoreProvider'
-import './i18n'
 
 ReactDOM.render( 
-  <Suspense fallback={'Loading...'}>
-    <StoreProvider>
-      <Scieski />
-    </StoreProvider>
-  </Suspense>,
+  <StoreProvider>
+    <Scieski />
+  </StoreProvider>,
   document.getElementById('root')
 )
